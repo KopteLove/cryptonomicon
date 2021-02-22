25e9b22f5376b6814233fe90a0ad16d5a114e956
KopteLove <V158694Koptelove>
2021-02-22 16:59:50 +0300

    Remove app.css and add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                class="block form-input w-full border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
               />
             </div>
@@ -196,5 +196,3 @@ export default {
   }
 };
 </script>
-
-<style src="./app.css"></style>
